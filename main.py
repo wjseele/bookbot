845fb1b23089ae07262a03509370f720a72e86e3
wjseele <wjseele@pm.me>
2025-06-05 11:08:05 +0200

    Got it working, hardcoded

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 def get_book_text(file_path):
     with open(file_path) as file:
         file_contents = file.read()
-        return file
+        return file_contents
 
 def main():
     book_to_read = "books/frankenstein.txt"
     book_text = get_book_text(book_to_read)
     print(book_text)
+
+main()
