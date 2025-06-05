76a3b0de29cfd73c54dfc1cbf9234848dda5db65
wjseele <wjseele@pm.me>
2025-06-05 11:13:10 +0200

    Working word counter

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ def get_book_text(file_path):
         file_contents = file.read()
         return file_contents
 
+def count_words(book_text):
+    words = book_text.split()
+    return len(words)
+
 def main():
     book_to_read = "books/frankenstein.txt"
     book_text = get_book_text(book_to_read)
-    print(book_text)
+    num_words = count_words(book_text)
+    print(f"{num_words} words found in the document")
 
 main()
