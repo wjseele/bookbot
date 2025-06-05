842ef0b6247b3ece9c5784e1f574b3a20aebc169
wjseele <wjseele@pm.me>
2025-06-05 14:29:32 +0200

    Added CLI arguments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import get_num_words
 from stats import get_num_chars
 from stats import sort_list
+import sys
 
 def get_book_text(file_path):
     with open(file_path) as file:
@@ -8,7 +9,13 @@ def get_book_text(file_path):
         return file_contents
 
 def main():
-    book_to_read = "books/frankenstein.txt"
+
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_to_read = sys.argv[1]
+    
     book_text = get_book_text(book_to_read)
     num_words = get_num_words(book_text)
     char_count = get_num_chars(book_text)
@@ -17,7 +24,7 @@ def main():
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_to_read}")
     print("----------- Word Count ----------")
-    print(f"{num_words} total words")
+    print(f"Found {num_words} total words")
     print("--------- Character Count -------")
 
     for letters in range(0, len(sorted_list)):
