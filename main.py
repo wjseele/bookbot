3460fee68d4b754dea98679581fd12877bc81e1c
wjseele <wjseele@pm.me>
2025-06-05 11:18:33 +0200

    Split up functions into multiple files

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
+from stats import get_num_words
+
 def get_book_text(file_path):
     with open(file_path) as file:
         file_contents = file.read()
         return file_contents
 
-def count_words(book_text):
-    words = book_text.split()
-    return len(words)
-
 def main():
     book_to_read = "books/frankenstein.txt"
     book_text = get_book_text(book_to_read)
-    num_words = count_words(book_text)
+    num_words = get_num_words(book_text)
     print(f"{num_words} words found in the document")
 
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(book_text):
+    words = book_text.split()
+    return len(words)
